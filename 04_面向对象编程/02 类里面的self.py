2e8adc2e83e776167e55388eb169592664594df0
yang-12345678 <1958523752@qq.com>
2021-03-21 14:33:02 +0800

    。

---
+++ 04_面向对象编程/02 类里面的self.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Date: 2021/03/21
+
+class Washer():
+    def wash(self):
+        print(self)
+
+
+hair = Washer()
+hair.wash()
+
+# 两个相同的地址
+print(hair)
--- 07_飞机大战/config/__init__.py
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-# Date: 2017/11/30
-
